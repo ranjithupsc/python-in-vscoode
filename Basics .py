9dde181be70af1d4f20c6feaed515de5f9ef3567
ranjithupsc <ranjithguru098@gmail.com>
2023-12-19 16:44:20 +0530

    commit

--- Basics .py
+++ Basics .py
@@ -1,2 +1,11 @@
-print("hello world")
-print("this is my 1st program")
+# Python3 program to add two numbers
+
+num1 = 15
+num2 = 12
+
+# Adding two nos
+import operator
+su = operator.add(num1,num2)
+
+# printing values
+print("Sum of {0} and {1} is {2}" .format(num1,num2, su))
